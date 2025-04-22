4f322514bc96744c2fa32c521a2150638764316e
virgin tarek <virgintarek476@gmail.com>
2025-04-22 16:16:55 +0200

    .

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-var ProductNameInput = document.getElementById("ProductNameInput").value
+var ProductNameInput = document.getElementById("ProductNameInput");
+var ProductPriceInput = document.getElementById("ProductPriceInput");
+var ProductCategoryInput = document.getElementById("ProductCategoryInput"); //ProductCategoryInput
+var ProductDescriptionInput = document.getElementById("ProductDescriptionInput");
+
 function addProduct() {
-    console.log(ProductNameInput);
+    var product = {
+        name: ProductNameInput.value,
+        price: ProductPriceInput.value,
+        category: ProductCategoryInput.value,
+        description: ProductDescriptionInput.value
+    }
 }
